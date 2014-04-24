05af5a8f71f627a8908b8ccfdb997da24f01c822
Kristiaan De Jongh <kristiaan.dejongh@scoyo.com>
2014-04-24 09:51:20 +0200

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,83 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Wrapper function around a matrix that caches the matrix inverse.
+    #
+    # Args:
+    #   x: the matrix that is wrapped, defaults to an empty matrix
+    #
+    # Returns:
+    #   a list of 4 functions to:
+    #       set/change the data of the wrapped matrix
+    #       get the data of the wrapped matrix
+    #       set the inverse of the wrapped matrix
+    #       get the (cached) inverse of the wrapped matrix
+    
+    # Error handling
+    if (!is.matrix(x) | (ncol(x) != nrow(x))) {
+        stop("'x' must be a square matrix")
+    }
+    
+    # initialize the inverse to 'NULL'
+    inv <- NULL
+    
+    set <- function(y) {
+        # Changes the data of the wrapped matrix x, the cached inverse is thereby reset to 'NULL'.
+        # Error handling
+        if (!is.matrix(y) | (ncol(y) != nrow(y))) {
+            stop("'y' must be a square matrix")
+        }
+        x <<- y      # set the data
+        inv <<- NULL # reset the (cached) inverse
+    }
+    get <- function() {
+        # Retrieves the wrapped matrix.
+        return(x) 
+    }
+    setInverse <- function(newInv) { 
+        inv <<- newInv 
+    }
+    getInverse <- function() { 
+        # Retrieves the cached inverse of the wrapped matrix; if no inverse was cached yet, 'NULL' is returned.
+        return(inv)
+    }
+    
+    list(set = set, 
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Returns the inverse of the matrix wrapped by 'x' (created by 'makeCacheMatrix'). The inverse is calculated the first time
+    # this function is called for a matrix and the calculated inverse is cached for following calls. 
+    # If the data of the matrix is changed the cached inverse will be reset to 'NULL' and calculated 
+    # 
+    # Args:
+    #   x: a list created by 'makeCacheMatrix' that wraps a matrix and contains functions to get and cache 
+    #       the inverse of that matrix
+    #
+    # Returns:
+    #   The inverse of the matrix wrapped by 'x'
+    
+    # Error handling
+    if (!is.list(x)) {
+        stop("'x' must be a list")
+    }
+    if (is.null(x$getInverse) | is.null(x$setInverse) | is.null(x$get)) {
+        stop("'x' is missing functions to get/set the inverse or get the matrix data.", 
+             " Call 'makeCacheMatrix' to create the proper argument")
+    }
+    
+    inv <- x$getInverse()
+    if (is.null(inv)) {
+        inv <- solve(x$get(), ...)
+        x$setInverse(inv)
+    } else {
+        message("getting cached inverse")
+    }
+    return(inv)
 }
